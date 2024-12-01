1dbf112cf5421816fdeaad3f5469f5cdd09f4903
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-12-01 11:53:24 +0000

    views(create artwork list view)

--- artworks/views.py
+++ artworks/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Artwork
+from .serializers import ArtworkSerializer
+from artly_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class ArtworkList(generics.ListCreateAPIView):
+    serializer_class = ArtworkSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    queryset = Artwork.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
