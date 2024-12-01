7941dfa35b1c64e4498dc84da66fce719f438149
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-12-01 12:00:06 +0000

    serializers(add image validation)

--- artworks/serializers.py
+++ artworks/serializers.py
@@ -8,6 +8,23 @@ class ArtworkSerializer(serializers.ModelSerializer):
     owner_id = serializers.ReadOnlyField(source='owner.id')
     sold = serializers.ReadOnlyField()
 
+    # validate_image function has been taken from the Code Institute DRF API
+    # walkthrough.
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image larger than 2MB.'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px.'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px.'
+            )
+        return value
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
