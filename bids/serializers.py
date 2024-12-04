8379b5f0538c3a3e05553a767779629d819754b2
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-12-04 19:21:33 +0000

    serializers(add additional fields and format code)

--- bids/serializers.py
+++ bids/serializers.py
@@ -1,19 +1,30 @@
 from rest_framework import serializers
 from .models import Bid
+from artworks.models import Artwork
 
 
 class BidSerializer(serializers.ModelSerializer):
     buyer = serializers.ReadOnlyField(source='buyer.username')
-    is_buyer = serializers.SerializerMethodField()
+    seller = serializers.SerializerMethodField()
+    status = serializers.SerializerMethodField()
+    artwork_title = serializers.SerializerMethodField()
 
+    def get_seller(self, obj):
+        return obj.artwork.owner.username
 
-    def get_is_owner(self, obj):
-        request = self.context['request']
-        return request.user == obj.owner
+    def get_status(self, obj):
+        return obj.artwork.sold
+
+    def get_artwork_title(self, obj):
+        return obj.artwork.artwork_title
 
     class Meta:
         model = Bid
         fields = [
-            'id', 'buyer', 'seller', 'artwork', 'bid_price', 'phone', 'email',
-            'status', 'created_at', 'updated_at',
+            'id', 'buyer', 'seller', 'artwork_title', 'artwork', 'bid_price',
+            'phone', 'email', 'status', 'created_at', 'updated_at'
         ]
+
+
+class BidDetailSerializer(BidSerializer):
+    artwork = serializers.ReadOnlyField(source='artwork.artwork_title')
