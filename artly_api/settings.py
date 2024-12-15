6a9ec32d2716a66c8ad3eaebf2e591f628503069
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-12-15 12:33:37 +0000

    settings(update Debug)

--- artly_api/settings.py
+++ artly_api/settings.py
@@ -55,8 +55,7 @@ JWT_AUTH_SAMESITE = 'None'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEV' in os.environ
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-laurakond-artlyapi-1p17b91siph.ws.codeinstitute-ide.net',
