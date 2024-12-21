8e5dade4ea4dd73d0e49305b6583dfeae3b1480c
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-12-21 07:31:50 +0000

    tests(uncomment code)

--- artworks/tests.py
+++ artworks/tests.py
@@ -56,76 +56,76 @@ class ArtworkListViewTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
-# class ArtworksDetailViewTests(APITestCase):
-#     def setUp(self):
-#         self.user_one = User.objects.create_user(
-#             username='user_one',
-#             password='password'
-#         )
-#         self.user_two = User.objects.create_user(
-#             username='user_two',
-#             password='password2'
-#         )
-#         Artwork.objects.create(
-#             owner=self.user_one,
-#             artwork_title='artwork title 1',
-#             description='artwork description 1',
-#             style='Other',
-#             type='Other',
-#             payment_method='Cash',
-#             price=20,
-#             contact='user1@email.com',
-#             location='user1',
-#             sold=False,
-#         )
-#         Artwork.objects.create(
-#             owner=self.user_two,
-#             artwork_title='artwork title 2',
-#             description='artwork description 2',
-#             style='Modern',
-#             type='Painting',
-#             payment_method='Cash',
-#             price=20,
-#             contact='user2@email.com',
-#             location='user2',
-#             sold=False,
-#         )
+class ArtworksDetailViewTests(APITestCase):
+    def setUp(self):
+        self.user_one = User.objects.create_user(
+            username='user_one',
+            password='password'
+        )
+        self.user_two = User.objects.create_user(
+            username='user_two',
+            password='password2'
+        )
+        Artwork.objects.create(
+            owner=self.user_one,
+            artwork_title='artwork title 1',
+            description='artwork description 1',
+            style='Other',
+            type='Other',
+            payment_method='Cash',
+            price=20,
+            contact='user1@email.com',
+            location='user1',
+            sold=False,
+        )
+        Artwork.objects.create(
+            owner=self.user_two,
+            artwork_title='artwork title 2',
+            description='artwork description 2',
+            style='Modern',
+            type='Painting',
+            payment_method='Cash',
+            price=20,
+            contact='user2@email.com',
+            location='user2',
+            sold=False,
+        )
 
-#     def test_can_retrieve_artwork_using_valid_id(self):
-#         response = self.client.get('/artworks/1/')
-#         self.assertEqual(response.data[
-#             'artwork_title'
-#         ], 'artwork title 1')
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_can_retrieve_artwork_using_valid_id(self):
+        response = self.client.get('/artworks/1/')
+        self.assertEqual(response.data[
+            'artwork_title'
+        ], 'artwork title 1')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-#     def test_user_cannot_retrieve_artwork_by_invalid_id(self):
-#         response = self.client.get('/artworks/100/')
-#         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    def test_user_cannot_retrieve_artwork_by_invalid_id(self):
+        response = self.client.get('/artworks/100/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-#     def test_user_can_update_owned_artwork(self):
-#         self.client.login(username='user_one', password='password')
-#         response = self.client.put(
-#             '/artworks/1/',
-#             {
-#                 'artwork_title': 'edit user one artwork title',
-#                 'description': 'artwork description',
-#                 'style': 'Other',
-#                 'type': 'Other',
-#                 'payment_method': 'Cash',
-#                 'price': 20.00,
-#                 'contact': 'user1@email.com',
-#                 'location': 'user1',
-#                 'sold': False,
-#             }
-#         )
-#         artwork = Artwork.objects.filter(pk=1).first()
-#         self.assertEqual(artwork.artwork_title, 'edit user one artwork title')
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_user_can_update_owned_artwork(self):
+        self.client.login(username='user_one', password='password')
+        response = self.client.put(
+            '/artworks/1/',
+            {
+                'artwork_title': 'edit user one artwork title',
+                'description': 'artwork description',
+                'style': 'Other',
+                'type': 'Other',
+                'payment_method': 'Cash',
+                'price': 20.00,
+                'contact': 'user1@email.com',
+                'location': 'user1',
+                'sold': False,
+            }
+        )
+        artwork = Artwork.objects.filter(pk=1).first()
+        self.assertEqual(artwork.artwork_title, 'edit user one artwork title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-#     def test_user_cannot_update_others_artwork(self):
-#         self.client.login(username='user_one', password='password')
-#         response = self.client.put(
-#             '/artworks/2/',
-#             {'artwork_title': 'a new title'}
-#         )
-#         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    def test_user_cannot_update_others_artwork(self):
+        self.client.login(username='user_one', password='password')
+        response = self.client.put(
+            '/artworks/2/',
+            {'artwork_title': 'a new title'}
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
