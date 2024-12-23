23c7eb0bad3a3436a6de4a04c74eaede83eed6de
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-12-23 15:05:48 +0000

    app(create profiles app and model)

---
+++ profiles/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+
+class Profile(models.Model):
+    """
+    Stores user profile information related to an individual user.
+    :model:'auth.User'
+    """
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=True)
+    location = models.CharField(max_length=255, blank=True)
+    images = models.ImageField(
+        upload_to='images/', default='../default_profile_nbsf4p'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+post_save.connect(create_profile, sender=User)
