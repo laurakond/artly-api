0cb728e82b546bde2126f4477439470b8d381244
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-12-28 16:30:53 +0000

    urls(edit saves url path)

--- saves/urls.py
+++ saves/urls.py
@@ -3,5 +3,5 @@ from saves import views
 
 urlpatterns = [
   path('saved/', views.SaveList.as_view()),
-  path('saved/<int:pk>', views.SaveDetail.as_view()),
+  path('saved/<int:pk>/', views.SaveDetail.as_view()),
 ]
