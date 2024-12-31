f573f7dcaddd2429bd888c8a00c39dff20bc9719
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-12-31 10:36:35 +0000

    views(add bid delete functionality)
    views(prevent user from bidding for already sold artwork)

--- bids/views.py
+++ bids/views.py
@@ -2,7 +2,6 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, permissions, status, filters
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError, PermissionDenied
-from artly_api.permissions import IsOwnerOrReadOnly, IsSellerOrReadOnly
 from .models import Bid, Artwork
 from .serializers import BidSerializer, BidDetailSerializer
 
@@ -40,6 +39,11 @@ class BidList(generics.ListCreateAPIView):
     ]
 
     def perform_create(self, serializer):
+        """
+        Applies two validations to prevent the seller to bid on their own
+        artwork, and for the buyer to submit a bid for the artwork that is
+        already sold.
+        """
         # get the validated artwork from the serializer as the serializer
         # is where that data is coming from
         artwork = serializer.validated_data['artwork']
@@ -48,10 +52,15 @@ class BidList(generics.ListCreateAPIView):
         if artwork.owner == self.request.user:
             raise ValidationError("You cannot bid on your own artwork.")
 
+        if artwork.sold:
+            raise ValidationError(
+                "This artwork is no longer available for purchase."
+            )
+
         instance = serializer.save(buyer=self.request.user)
 
 
-class BidDetail(generics.RetrieveUpdateAPIView):
+class BidDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Displays the buyer's bid, and edits(seller only) a bid that belongs to the
     seller. Evaluates who the logged in user is and prevents the buyer from
@@ -59,7 +68,6 @@ class BidDetail(generics.RetrieveUpdateAPIView):
     """
     serializer_class = BidDetailSerializer
     permission_classes = [
-        IsSellerOrReadOnly,
         permissions.IsAuthenticatedOrReadOnly
     ]
     queryset = Bid.objects.all()
@@ -68,8 +76,8 @@ class BidDetail(generics.RetrieveUpdateAPIView):
         obj = super().get_object()
         user = self.request.user
 
-        # Logic to prevent the buyer from editing their bid.
-        if user != obj.seller:
+        # Logic to prevent the wrong user(not logged in) to edit a bid.
+        if user != obj.seller and user != obj.buyer:
             raise PermissionDenied(
                 {"message": "You don't have access to modify this bid."}
             )
@@ -77,20 +85,29 @@ class BidDetail(generics.RetrieveUpdateAPIView):
 
     def put(self, request, *args, **kwargs):
         """
-        Takes each bid instance, evaluates, updates the bid status and the
-        artwork's sold field. Depending on the status, it will send appropriate
-        message to the user and disable the bid functionality if the artwork
-        object is sold.
-        This part of the code was written with the help of DRF REST
-        documentation and parts of it were appropriated from a fellow student's
-        work. See the README.md for full credit/information.
+        Put method for the seller of the artwork. Takes each bid instance,
+        evaluates, and updates the bid status and the Artwork model's sold
+        field.
+        Depending on the status, it will send appropriate message to the user
+        and disable the bid functionality if the artwork object is sold.
+        The put method was written with the help of DRF REST documentation and
+        parts of it were appropriated from a fellow student's project.
+        See the README.md for full credit/information.
         """
 
         instance = self.get_object()
         initial_status = instance.status
+        user = self.request.user
 
-        # Check if the initial artwork status is sold in both Bid and Artwork
-        # models, and if so, throw a message stating that.
+        # Checks that the user who can edit the bids status is the seller of
+        # the artwork
+        if user != instance.seller:
+            raise PermissionDenied({
+                "message": "Only the seller can modify this bid."
+            })
+
+        # Checks if the initial artwork status is 'sold' in both Bid and
+        # Artwork models, and if so, throw a message stating that.
         if initial_status == "Sold" or instance.artwork.sold:
             return Response(
                 {"message": "Artwork is no longer available for purchase."},
@@ -108,7 +125,7 @@ class BidDetail(generics.RetrieveUpdateAPIView):
         # extract artwork status
         updated_status = serializer.validated_data.get('status')
 
-        # Updates Artwork sold field to true if the bid status is sold.
+        # Updates Artwork sold field to true if the bid status is 'sold'.
         if updated_status == "Sold":
             instance.artwork.sold = True
             instance.artwork.save()
@@ -127,3 +144,23 @@ class BidDetail(generics.RetrieveUpdateAPIView):
         self.perform_update(serializer)
 
         return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        """
+        Validates the user and allows only the owner of the bid to
+        delete it.
+        """
+        instance = self.get_object()
+        user = self.request.user
+
+        # Checks that the user is the buyer and throws an error message if not
+        if user != instance.buyer:
+            raise PermissionDenied(
+                {"message": "Only the buyer can delete this bid."}
+            )
+
+        self.perform_destroy(instance)
+        return Response(
+            {"message": "Bid deleted successfully."},
+            status=status.HTTP_204_NO_CONTENT
+        )
