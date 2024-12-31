04635c35748cd5a2d9f3fd392864009f97929e8e
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-12-31 10:37:42 +0000

    permissions(remove redundant code)

--- artly_api/permissions.py
+++ artly_api/permissions.py
@@ -11,14 +11,3 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.owner == request.user
-
-
-class IsSellerOrReadOnly(permissions.BasePermission):
-    """
-    Checks if the user is the seller of the artwork, if not, only gives
-    Read-only access.
-    """
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.seller == request.user
